aedf4e9886c65fc108472aaafe0fe7c0467bd849
Dan Joshua Naing <joshuanaing212@gmail.com>
2023-07-09 14:45:20 +0800

    update
    added 1st project

--- src/About.js
+++ src/About.js
@@ -4,23 +4,77 @@ import animationData from './assets/programmer.json';
 
 const About = () => {
   return (
-    <div className="About-contents flex items-center justify-center min-screen mb-20" style={{ height: '50vh' }} id='about'>
-      <div className="max-w-3xl mx-auto px-7">
-        <div className="flex flex-col md:flex-row items-center">
-          <div className="mr-10 mb-6 md:mb-0">
-            <div style={{ width: '300px', height: '300px' }}>
-              <Lottie animationData={animationData} />
+    <div class="About-contents">
+        <div className=" flex items-center justify-center min-screen mb-20" style={{ height: '50vh' }} id='about'>
+              <div className="max-w-3xl mx-auto px-7">
+                <div className="flex flex-col md:flex-row items-center">
+                  <div className="mr-10 mb-6 md:mb-0">
+                    <div style={{ width: '300px', height: '300px' }}>
+                      <Lottie animationData={animationData} />
+                    </div>
+                  </div>
+                  <div>
+                    <h1 className="text-4xl font-bold mb-4 text-blue-500">About Me</h1>
+                    <p className="text-lg text-gray-600 text-justify">
+                      Hi there! I'm <b>Dan Joshua Naing</b>, a fresh graduate of BSIT from STI Global City. I have a strong foundation in C#, Java, Game Development, HTML, CSS, React, Tailwind, and SQL. I am constantly learning and seeking new challenges to enhance my skills and contribute to innovative projects. Let's connect and create something amazing together!
+                    </p>
+                    
+                  </div>
+                  
+                </div>
+              </div>
+
             </div>
-          </div>
-          <div>
-            <h1 className="text-4xl font-bold mb-4 text-blue-500">About Me</h1>
-            <p className="text-lg text-gray-600 text-justify">
-              Hi there! I'm <b>Dan Joshua Naing</b>, a fresh graduate of BSIT from STI Global City. I have a strong foundation in C#, Java, Game Development, HTML, CSS, React, Tailwind, and SQL. I am constantly learning and seeking new challenges to enhance my skills and contribute to innovative projects. Let's connect and create something amazing together!
-            </p>
-          </div>
-        </div>
+            <div className="container my-24 mx-auto md:px-6">
+  <section className="mb-32 text-center">
+    <h2 className="m-16 mt-16 pt-20 text-4xl font-bold text-blue-500">
+      Tech Stack
+    </h2>
+
+    <div className="grid items-center gap-6 grid-cols-2 md:grid-cols-4 max-w-3xl mx-auto">
+      <div className="mb-12 md:mb-0">
+        <img src="./images/cSharpicon.png"
+          className="px-6 dark:brightness-150 md:px-12 grayscale" alt="C# - logo" />
+      </div>
+
+      <div className="mb-12 md:mb-0">
+        <img src="./images/react.png"
+          className="px-6 dark:brightness-150 md:px-12 grayscale" alt="react - logo" />
+      </div>
+
+      <div className="mb-12 md:mb-0">
+        <img src="./images/tailwind.png"
+          className="px-6 dark:brightness-150 md:px-12 grayscale" alt="Tailwind - logo" />
+      </div>
+
+      <div className="mb-12 md:mb-0">
+        <img src="./images/unity.png"
+          className="px-6 dark:brightness-150 md:px-12 grayscale" alt="unity - logo" />
+      </div>
+
+      <div className="mb-12 md:mb-0 hidden md:block">
+     
+      </div>
+      
+      <div className="mb-12 md:mb-0">
+        <img src="./images/html.png"
+          className="px-6 dark:brightness-150 md:px-12 grayscale mb-20" alt="html - logo" />
+      </div>
+      
+      <div className="mb-12 md:mb-0 ">
+        <img src="./images/css.png"
+          className="px-6 dark:brightness-150 md:px-12 grayscale mb-20" alt="Css - logo" />
+      </div>
+
+      <div className="mb-12 md:mb-0 hidden md:block" >
+       
       </div>
     </div>
+  </section>
+</div>
+            </div>
+   
+   
   );
 };
 
--- src/Projects.js
+++ src/Projects.js
@@ -3,11 +3,12 @@ import React from 'react';
 const Projects = () => {
   const projects = [
     {
-      title: 'Project 1',
-      description: 'Lorem ipsum dolor sit ametsdfjalksjdal, consectetur adipiscing elit.',
-      imageUrl: '/images/misosoup.png',
+      title: 'Akumi and the Forbidden Language',
+      description: 'A 2D Isometric Role Playing Android Game genre that will easily lead newbies and interested individuals through the game—educating people on how to code by having them play the game and read the story with instructions so they can completely understand the game mechanics.',
+      imageUrl: '/images/akumiTitle.png',
       link: 'https://example.com/project1',
       linkSource: 'github.com',
+      icons:'fa-brands fa-unity ',
     },
     {
       title: 'Project 2',
@@ -28,13 +29,13 @@ const Projects = () => {
 
   return (
     <div className="Projects-contents flex flex-col items-center justify-start min-h-screen mt-20" id='project'>
-      <div className="max-w-3xl mx-auto px-4">
+      <div className="max-w-5xl mx-auto px-4">
         <h1 className="text-4xl font-bold mb-8 text-blue-500 mt-20">Projects</h1>
         <h1 className="text-4sm font-bold mb-4 text-gray-600">Projects made by me! </h1>
         {projects.map((project, index) => (
              <div key={index} className="bg-white shadow-lg rounded-lg overflow-hidden mb-6 mt-20">
              <div className="flex flex-col md:flex-row">
-               <div className="w-full md:w-48 h-48">
+               <div className="w-full md:w-51 h-48">
                  <img className="w-full h-full object-cover" src={project.imageUrl} alt="Card" />
                </div>
                <div className="flex flex-col justify-between p-4">
@@ -42,6 +43,10 @@ const Projects = () => {
                    <h2 className="text-xl font-bold mb-2">{project.title}</h2>
                    <p className="text-gray-600 flex-grow">{project.description}</p>
                  </div>
+                 <div className='text-3xl md:text-4xl pl-28'>
+                 <i class={project.icons} aria-hidden="true"></i>
+                  </div>
+                 
                  <div className="flex mt-4">
                    <a href={project.link} className=" text-gray-800 font-bold py-2 px-4 rounded hover:text-blue-600 transition duration-300 ease-in-out mr-2">
                      Code <i className="fa fa-github" aria-hidden="true"></i>
